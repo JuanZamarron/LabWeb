d1dcf3926cb1fae1d704b4bd5504577342b6d55e
JuanZamarron <47125895+JuanZamarron@users.noreply.github.com>
2019-04-08 17:33:23 -0500

    Update app.js

--- EX2/app.js
+++ EX2/app.js
@@ -1,12 +1,37 @@
+const express = require('express')
 const metjs = require('./met.js')
+
+const app = express()
 
 const search = 'sunflowers'
 
+app.get('/met', function(req, res){
+    res.setHeader('Access-Control.Allow-Origin', '*');
+    if (!req.query.search){
+        return res.send({
+            error: "ERROR"
+        })
+    }
+    metjs.met(req.query.search, function(error, response){
+        if(error){
+            return res.send({
+                error: error
+            })
+        }
+        return res.send(response)
+    })
+})
+
+app.listen('3000', function(){
+    console.log('Informacion')
+})
+
+/*
 metjs.met(search, function(error, response){
     if(error){
         console.log(error)
     }else{
-        metjs.met1(response, function(error, response){
+        metjs.met1(response.data, function(error, response){
             if (error){
                 console.log(error)
             } else {
@@ -15,3 +40,4 @@ metjs.met(search, function(error, response){
         })
     }
 })
+*/
